89e286a2ef4f8fcf0c06978f10eb490a5c48d960
Kzhao <ken.zh77@gmail.com>
2020-02-24 12:35:35 -0500

    commetns

--- src/index.js
+++ src/index.js
@@ -6,22 +6,40 @@ import ReactDOM from 'react-dom';
 
 //create a react component
 
+// const App = () => {
+//     return (
+//         <div>
+//             <label className ="label">
+//                 Enter Name:
+//             </label>
+//             <input id = "name" type ="text"></input>
+//             <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+//         </div>
+//     )
+// }
+
 const App = () => {
     return (
-        <div>
-            <label className ="label">
-                Enter Name:
-            </label>
-            <input id = "name" type ="text"></input>
-            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+        <div className="ui container comments">
+            <div className="comment">
+                <a href = "/" className="avatar">
+                    <img alt= "avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className = "metadata">
+                        <span className = "date"> Today at 6:00PM</span>
+                    </div>
+                    <div className="text"> Nice blog post!</div>
+                </div>
+
+            </div>
         </div>
     )
 }
 
 // Take the react component and show it on the screen 
 
-ReactDOM.render(
-    <App />, 
-    document.querySelector('#root')
-
-)
+ReactDOM.render(<App />, document.querySelector('#root'))
